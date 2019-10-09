c5e8fd5cb7399d816450af864e57017f345d70d7
mpollayil <mathewjosepollayil@gmail.com>
2019-10-09 17:01:51 +0200

    Added the modification of the trajectory header stamp for solving the points dropping issue

--- panda_softhand_control/src/HandControl.cpp
+++ panda_softhand_control/src/HandControl.cpp
@@ -45,6 +45,9 @@ bool HandControl::sendHandTrajectory(trajectory_msgs::JointTrajectory trajectory
         return false;
     }
 
+    // Setting the most recent time to the trajectory header
+    trajectory.header.stamp = ros::Time::now();
+
 	// Send the message and wait for the result
 	control_msgs::FollowJointTrajectoryGoal goalmsg;
 	goalmsg.trajectory = trajectory;
